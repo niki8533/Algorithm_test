3a4454cd7fb7c28b222f495db0e133b94725f9ad
Sehyun Jin <75302306+niki8533@users.noreply.github.com>
2025-06-27 18:56:17 +0900

    [Bronze II] Title: 진법 변환, Time: 104 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Bronze/2745. 진법 변환/진법 변환.java
+++ 백준/Bronze/2745. 진법 변환/진법 변환.java
@@ -1,31 +1,29 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		String s;
-		int B;
-		char c;
-		int answer = 0;
-		int temp = 1;
-		
-		s = sc.next();
-		B = sc.nextInt();
-				
-		for(int i = s.length() -1 ; i >= 0  ; i--) {
-			
-			c = s.charAt(i);
-			if(c >= 'A' && c <= 'Z') {
-				answer = answer + (c - 55) * temp;
-			}
-			else
-				answer = answer + (c - 48) * temp;
-			
-			temp = temp * B;
-		}
-		
-		System.out.println(answer);
-		
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str, " ");
+
+        String N = st.nextToken();
+        int B = Integer.parseInt(st.nextToken());
+        int answer = 0;
+        int temp = 1;
+
+         for(int i = N.length() - 1 ; i >= 0 ; i--){
+            char c = N.charAt(i);
+            if(c >= 'A' && c <= 'Z') {
+                answer = answer + (c - 55) * temp;
+            }
+            else
+                answer = answer + (c - 48) * temp;
+            temp *= B;
+        }
+
+        System.out.println(answer);
+    }
 }
