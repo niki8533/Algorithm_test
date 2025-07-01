684bc99da18d846b945ba8944e90c2c772687822
Sehyun Jin <75302306+niki8533@users.noreply.github.com>
2025-07-01 16:44:58 +0900

    [level 2] Title: 주식가격, Time: 77.44 ms, Memory: 80.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] prices) {
+        int[] answer = {};
+        
+        answer = new int[prices.length];
+        for(int i = 0 ; i < prices.length - 1 ; i++){
+            Stack<Integer> stack = new Stack<>();
+            for(int j = i+1 ; j < prices.length ; j++){
+                stack.add(prices[j]);
+                if(prices[i]> prices[j]){
+                    break;
+                }
+            }
+            answer[i] = stack.size();
+        }
+
+        answer[prices.length - 1] = 0;
+        
+        return answer;
+    }
+}
