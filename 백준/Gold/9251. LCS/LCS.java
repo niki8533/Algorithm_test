b4b28d7b1837ef679032fed693386470d5429e4f
Sehyun Jin <niki8533@naver.com>
2025-09-01 17:41:00 +0900

    [Gold V] Title: LCS, Time: 188 ms, Memory: 28896 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static Integer[][] dp;
+    static char[] str1;
+    static char[] str2;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        str1 = br.readLine().toCharArray();
+        str2 = br.readLine().toCharArray();
+
+        dp = new Integer[str1.length][str2.length];
+
+        int x = str1.length - 1;
+        int y = str2.length - 1;
+        System.out.println(LCS(str1.length - 1, str2.length - 1));
+    }
+
+    static int LCS(int x, int y){
+        if(x == -1 || y == -1){
+            return 0;
+        }
+
+
+        if(dp[x][y] == null){
+            dp[x][y] = 0;
+
+            if(str1[x] == str2[y]){
+                dp[x][y] = LCS(x - 1, y - 1) + 1;
+            }
+            else{
+                dp[x][y] = Math.max(LCS(x-1, y), LCS(x, y-1));
+            }
+        }
+
+        return dp[x][y];
+    }
+}
