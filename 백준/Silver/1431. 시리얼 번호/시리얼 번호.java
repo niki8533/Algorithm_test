94c27e8d55279ecc5d011d7e4e45dbf64f10c8f1
Sehyun Jin <niki8533@naver.com>
2025-09-23 18:00:07 +0900

    [Silver III] Title: 시리얼 번호, Time: 104 ms, Memory: 14300 KB -BaekjoonHub

---
+++ 백준/Silver/1431. 시리얼 번호/시리얼 번호.java
@@ -0,0 +1,48 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        String[] arr = new String[N];
+        for(int i = 0 ; i < N ; i++){
+            arr[i] = br.readLine();
+        }
+
+        Arrays.sort(arr, (o1, o2) -> {
+            if(o1.length() != o2.length()){
+                return o1.length() - o2.length();
+            } else {
+                int sum1 = 0;
+                int sum2 = 0;
+
+                for(int i = 0 ; i < o1.length() ; i++){
+                    char c1 = o1.charAt(i);
+                    char c2 = o2.charAt(i);
+
+                    if(c1 >= 48 && c1 <= 57) {
+                        sum1 = sum1 + c1 - '0';
+                    }
+                    if(c2 >= 48 && c2 <= 57) {
+                        sum2 = sum2 + c2 - '0';
+                    }
+                }
+
+                if(sum1 == sum2){
+                    return o1.compareTo(o2);
+                } else{
+                    return sum1 - sum2;
+                }
+            }
+        });
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0 ; i < N ; i++){
+            sb.append(arr[i]).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
