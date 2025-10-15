82f3568130207bba8bc8b3441b6c930694f34bb0
Sehyun Jin <niki8533@naver.com>
2025-10-15 18:22:54 +0900

    [Gold V] Title: 전깃줄, Time: 100 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Gold/2565. 전깃줄/전깃줄.java
+++ 백준/Gold/2565. 전깃줄/전깃줄.java
@@ -4,7 +4,6 @@ import java.io.*;
 public class Main {
     static int[][] arr;
     static Integer[] dp;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -12,9 +11,10 @@ public class Main {
 
         arr = new int[N][2];
         dp = new Integer[N];
-
+        
         for(int i = 0 ; i < N ; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
+
             arr[i][0] = Integer.parseInt(st.nextToken());
             arr[i][1] = Integer.parseInt(st.nextToken());
         }
@@ -24,9 +24,8 @@ public class Main {
         });
 
         int max = 0;
-
         for(int i = 0 ; i < N ; i++){
-            max = Math.max(recur(i), max);
+            max = Math.max(max, recur(i));
         }
 
         System.out.println(N - max);
@@ -37,7 +36,7 @@ public class Main {
         if(dp[N] == null){
             dp[N] = 1;
 
-            for(int i = N + 1 ; i < dp.length ; i++){
+            for(int i = N+1 ; i < dp.length ; i++){
                 if(arr[N][1] < arr[i][1]){
                     dp[N] = Math.max(dp[N], recur(i) + 1);
                 }
