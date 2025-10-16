9f314a3559c8df80a505d98852f31adcb312ef93
Sehyun Jin <niki8533@naver.com>
2025-10-16 16:54:44 +0900

    [Silver IV] Title: 주몽, Time: 168 ms, Memory: 16492 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.java
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -7,15 +7,15 @@ public class Main {
 
         int N = Integer.parseInt(br.readLine());
         int M = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
 
+        int[] arr = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i = 0 ; i < N ; i++){
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
         Arrays.sort(arr);
-
+        
         int left = 0;
         int right = N - 1;
         int answer = 0;
