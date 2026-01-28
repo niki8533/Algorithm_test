bcc0c0c9dda798c392f0fa151a0962e012d4822a
Sehyun Jin <niki8533@naver.com>
2026-01-28 18:16:31 +0900

    [Silver IV] Title: 스택, Time: 160 ms, Memory: 18848 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -1,43 +1,54 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Stack<Integer> stack = new Stack<>();
+	public static void main(String[] args) throws IOException {
+		Stack<Integer> stack = new Stack<>();
 
-        int N = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0 ; i < N ; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int num = Integer.parseInt(br.readLine());
 
-            String command = st.nextToken();
-            switch(command){
-                case "push":
-                    int temp = Integer.parseInt(st.nextToken());
-                    stack.push(temp);
-                    break;
-                case "pop":
-                    if(stack.isEmpty())
-                        sb.append(-1 + "\n");
-                    else sb.append(stack.pop() + "\n");
-                    break;
-                case "size":
-                    sb.append(stack.size() + "\n");
-                    break;
-                case "empty":
-                    if(stack.isEmpty())
-                        sb.append(1 + "\n");
-                    else sb.append(0 + "\n");
-                    break;
-                case "top":
-                    if(stack.isEmpty())
-                        sb.append(-1 + "\n");
-                    else sb.append(stack.peek() + "\n");
-                    break;
-            }
-        }
+		StringTokenizer st;
+		for(int i = 0 ; i < num ; i++){
+			st = new StringTokenizer(br.readLine(), " ");
+			String command = st.nextToken();
 
-        System.out.println(sb);
-    }
+			switch (command) {
+				case "push":
+					stack.push(Integer.parseInt(st.nextToken()));
+					break;
+
+				case "pop":
+					if(stack.isEmpty())
+						sb.append(-1 + "\n");
+					else
+						sb.append(stack.pop()).append("\n");
+					break;
+
+				case "size":
+					sb.append(stack.size()).append("\n");
+					break;
+
+				case "empty":
+					if(stack.isEmpty())
+						sb.append(1 + "\n");
+					else
+						sb.append(0 + "\n");
+					break;
+
+				case "top":
+					if(stack.isEmpty())
+						sb.append(-1 + "\n");
+					else
+						sb.append(stack.peek()).append("\n");
+					break;
+			}
+		}
+
+		System.out.print(sb);
+	}
 }
