5787dc55b09b3c08fb4f8843cdb8f4149bb8cbad
Sehyun Jin <niki8533@naver.com>
2026-01-29 15:03:40 +0900

    [Silver IV] Title: 괄호, Time: 116 ms, Memory: 14476 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int num = Integer.parseInt(br.readLine());
+
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0 ; i < num ; i++){
+			String ps = br.readLine();
+
+			Stack<Character> stack = new Stack<>();
+			for(int j = 0 ; j < ps.length() ; j++) {
+				char c = ps.charAt(j);
+				if( c == '(' || stack.isEmpty())
+					stack.add(c);
+				else
+					if(!stack.isEmpty() && stack.peek() == '(')
+						stack.pop();
+			}
+
+			if(stack.isEmpty())
+				sb.append("YES\n");
+			else
+				sb.append("NO\n");
+		}
+
+		System.out.print(sb);
+	}
+}
