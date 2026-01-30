fa177794a6b66e06824ed641133b9dc0698f5ec1
Sehyun Jin <niki8533@naver.com>
2026-01-30 15:08:20 +0900

    [Silver IV] Title: 카드2, Time: 160 ms, Memory: 45612 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void  main(String[] args) throws IOException{
+		Queue<Integer> q = new LinkedList<>();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+
+		for(int i = 0 ; i < N ; i++){
+			q.offer(i+1);
+		}
+
+		while(q.size() > 1) {
+			q.poll();
+			q.offer(q.poll());
+		}
+
+		System.out.println(q.poll());
+	}
+}
