581881d998efe2251786d47959c287138c301b56
Sehyun Jin <niki8533@naver.com>
2026-02-12 18:09:41 +0900

    [Silver III] Title: 프린터 큐, Time: 132 ms, Memory: 15148 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.java
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.java
@@ -1,52 +1,54 @@
 import java.util.*;
+import java.io.*;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		
-		
-		int test_case = sc.nextInt();
-		
-		
-		for(int t = 0 ; t < test_case ; t++) {
-			
-			int N = sc.nextInt();
-			int M = sc.nextInt();
-			Queue<int[]> q = new LinkedList<int[]>();
-			
-			for(int i = 0 ; i < N ; i++) {
-				q.offer(new int[] {i, sc.nextInt()});
+
+		int testCase = Integer.parseInt(br.readLine());
+
+		for(int t = 0 ; t < testCase ; t++){
+			LinkedList<int[]> q = new LinkedList<>();
+
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			int N = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
+
+			st = new StringTokenizer(br.readLine());
+			for(int i = 0 ; i < N ; i++){
+				q.add(new int[] {i, Integer.parseInt(st.nextToken())});
 			}
-			
+
 			int count = 0;
-			
-			while(!q.isEmpty()) {
+			while(!q.isEmpty()){
 				int[] front = q.poll();
 				boolean isMax = true;
-				
-				for(int i = 0 ; i < q.size(); i++) {
-					if(front[1] <  ((LinkedList<int[]>) q).get(i)[1]) {
+
+				for(int i = 0 ; i < q.size() ; i++){
+					if(front[1] < q.get(i)[1]) {
 						q.offer(front);
-						for(int j = 0 ; j < i ; j++) {
+						for (int j = 0; j < i; j++) {
 							q.offer(q.poll());
 						}
 						isMax = false;
 						break;
 					}
 				}
-				if(isMax == false) {
+
+				if(!isMax){
 					continue;
 				}
-				
+
 				count++;
-				if(front[0] == M) {
+				if(front[0] == M){
 					break;
 				}
 			}
-			sb.append(count).append('\n');
+
+			sb.append(count).append("\n");
 		}
-		
+
 		System.out.println(sb);
 	}
 }
