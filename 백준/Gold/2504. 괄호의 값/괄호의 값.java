01abbeaf92057abff3d6bd5c4cd13bae04dd8b46
Sehyun Jin <niki8533@naver.com>
2026-03-03 13:43:31 +0900

    [Gold V] Title: 괄호의 값, Time: 104 ms, Memory: 14304 KB -BaekjoonHub

--- 백준/Gold/2504. 괄호의 값/괄호의 값.java
+++ 백준/Gold/2504. 괄호의 값/괄호의 값.java
@@ -4,50 +4,52 @@ import java.io.*;
 public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String str = br.readLine();
 
-		int count = 1;
+		int temp = 1;
 		int answer = 0;
-
 		Stack<Character> stack = new Stack<>();
-
+		String str = br.readLine();
 		for(int i = 0 ; i < str.length() ; i++){
 			char c = str.charAt(i);
 
 			if(c == '('){
 				stack.push(c);
-				count *= 2;
+				temp = temp * 2;
 			}
-			else if(c == '['){
+
+			if(c == '['){
 				stack.push(c);
-				count *= 3;
+				temp = temp * 3;
 			}
-			else if(c == ')'){
+
+			if(c == ')'){
 				if(stack.isEmpty() || stack.peek() != '('){
 					answer = 0;
 					break;
-				} else if(str.charAt(i-1) == '('){
-					answer = answer + count;
 				}
-				count /= 2;
+				else if(str.charAt(i-1) == '('){
+					answer = temp + answer;
+				}
+				temp = temp / 2;
 				stack.pop();
 			}
 			else if(c == ']'){
 				if(stack.isEmpty() || stack.peek() != '['){
 					answer = 0;
 					break;
-				} else if(str.charAt(i-1) == '['){
-					answer = answer + count;
 				}
-				count /= 3;
+				else if(str.charAt(i-1) == '['){
+					answer = temp + answer;
+				}
+				temp = temp / 3;
 				stack.pop();
 			}
 		}
 
-		if(!stack.isEmpty()){
+        if(!stack.isEmpty()){
 			answer = 0;
 		}
-
+        
 		System.out.println(answer);
 	}
 }
