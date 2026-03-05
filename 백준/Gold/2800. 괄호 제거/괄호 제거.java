a82bcc467b7e1b18373b9bbe59f82e8fe5b4ab57
Sehyun Jin <niki8533@naver.com>
2026-03-05 16:10:01 +0900

    [Gold IV] Title: 괄호 제거, Time: 168 ms, Memory: 18092 KB -BaekjoonHub

--- 백준/Gold/2800. 괄호 제거/괄호 제거.java
+++ 백준/Gold/2800. 괄호 제거/괄호 제거.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	static List<int[]> brackets;
+	static List<int[]> 	list = new ArrayList<>();
 	static boolean[] check;
 	static Set<String> set;
 
@@ -11,14 +11,13 @@ public class Main {
 		String str = br.readLine();
 
 		Stack<Integer> stack = new Stack<>();
-		brackets = new ArrayList<>();
+
 		for(int i = 0 ; i < str.length() ; i++){
 			char c = str.charAt(i);
-
 			if(c == '('){
 				stack.push(i);
 			} else if(c == ')'){
-				brackets.add(new int[] {stack.pop(), i});
+				list.add(new int[]{stack.pop(), i});
 			}
 		}
 
@@ -32,14 +31,14 @@ public class Main {
 	}
 
 	static void comb(int depth, char[] chr){
-		if(depth == brackets.size()){
+		if(depth == list.size()){
 			boolean f = false;
 			StringBuilder sb = new StringBuilder();
-
 			for(int i = 0 ; i < chr.length ; i++){
 				if(!check[i]){
 					sb.append(chr[i]);
-				} else{
+				}
+				else {
 					f = true;
 				}
 			}
@@ -47,15 +46,16 @@ public class Main {
 			if(f){
 				set.add(sb.toString());
 			}
+
 			return;
 		}
 
+		int[] bracket = list.get(depth);
 		comb(depth + 1, chr);
-		int[] bracket = brackets.get(depth);
 		check[bracket[0]] = true;
 		check[bracket[1]] = true;
 
-		comb(depth+1, chr);
+		comb(depth + 1, chr);
 		check[bracket[0]] = false;
 		check[bracket[1]] = false;
 	}
