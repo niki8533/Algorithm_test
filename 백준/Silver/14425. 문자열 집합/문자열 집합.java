ab58cf3d7fb41995ac6649af48d3f940c3a28b5d
Sehyun Jin <niki8533@naver.com>
2026-03-11 00:22:31 +0900

    [Silver IV] Title: 문자열 집합, Time: 364 ms, Memory: 31452 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.java
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -2,25 +2,32 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+
+
 
 		HashSet<String> set = new HashSet<>();
-
-        for(int i = 0 ; i < N ; i++){
+		for(int i = 0 ; i < N ; i++){
 			set.add(br.readLine());
-        }
+		}
 
-        int answer = 0;
-        for(int i = 0 ; i < M ; i++){
-           if(set.contains(br.readLine()))
-               answer++;
-        }
+		Stack<String> stack = new Stack<>();
+		for(int i = 0 ; i < M ; i++){
+			stack.push(br.readLine());
+		}
 
-        System.out.println(answer);
-    }
+		int answer = 0;
+		while(!stack.isEmpty()){
+			if(set.contains(stack.pop())){
+				answer++;
+			}
+		}
+
+		System.out.println(answer);
+	}
 }
