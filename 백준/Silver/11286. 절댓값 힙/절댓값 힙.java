8c83c6d55409217ee58c7d22c4f67dabce018dd0
Sehyun Jin <niki8533@naver.com>
2026-03-13 15:55:35 +0900

    [Silver I] Title: 절댓값 힙, Time: 292 ms, Memory: 27144 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -1,37 +1,38 @@
-import java.util.PriorityQueue;
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		StringBuilder sb = new StringBuilder();
-		
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+
 		PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> {
 			int abs1 = Math.abs(o1);
 			int abs2 = Math.abs(o2);
-			
-			if(abs1 > abs2)
+
+			if(abs1 > abs2){
 				return abs1 - abs2;
-			else if(abs1 == abs2)
+			} else if(abs1 == abs2){
 				return o1 - o2;
-			else
-				return -1;
+			} else return -1;
 		});
-		
-		int N = sc.nextInt();
-		
-		for(int i = 0 ; i < N ; i++) {
-			int x = sc.nextInt();
-			
-			if(x == 0)
-				if(pq.isEmpty())
-					sb.append(0).append('\n');
-				else
-					sb.append(pq.poll()).append('\n');
-			else
+
+		StringBuilder sb = new StringBuilder();
+
+		for(int i = 0 ; i < N ; i++){
+			int x = Integer.parseInt(br.readLine());
+
+			if(x == 0){
+				if(pq.isEmpty()){
+					sb.append(0 + "\n");
+				} else {
+					sb.append(pq.poll() + "\n");
+				}
+			} else {
 				pq.add(x);
+			}
 		}
-		
+
 		System.out.println(sb);
 	}
 }
