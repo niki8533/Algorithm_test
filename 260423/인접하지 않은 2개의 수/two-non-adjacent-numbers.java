31efa8c6c8129ca030558ce4846584180e9da8ca
Sehyun Jin <niki8533@naver.com>
2026-04-23 16:59:32 +0900

    Title: 인접하지 않은 2개의 수, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260423/인접하지 않은 2개의 수/two-non-adjacent-numbers.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0 ; i < N ; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int max = 0;
+        for(int i = 0 ; i < N - 2 ; i++){
+            for(int j = i + 1 ; j < N ; j++){
+                max = Math.max(max, arr[i] + arr[j]);
+            }
+        }
+
+        System.out.println(max);
+    }
+}
