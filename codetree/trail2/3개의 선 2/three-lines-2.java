b75073f23d129b644946baa4abed78182c8f50be
Sehyun Jin <niki8533@naver.com>
2026-05-27 00:46:20 +0900

    [Wrong Answer] 3개의 선 2 (96ms, 9MB)

--- codetree/trail2/3개의 선 2/three-lines-2.java
+++ codetree/trail2/3개의 선 2/three-lines-2.java
@@ -16,24 +16,50 @@ public class Main {
         int answer = 0;
         for(int i = 0 ; i <= 10 ; i++){
             for(int j = 0 ; j <= 10 ; j++){
-                boolean canPass = true;
                 for(int k = 0 ; k <= 10 ; k++){
-
+                    boolean canPass = true;
                     for(int l = 0 ; l < N ; l++){
-                        if(arr[l][0] == i || arr[l][0] == j || arr[l][0] == k){
-                            canPass = true;
+                        if(arr[l][0] != i || arr[l][0] != j || arr[l][0] != k){
+                            canPass = false;
+                            break;
                         }
-                        else if(arr[l][1] == i || arr[l][1] == j || arr[l][1] == k){
-                            canPass = true;
-                        }
-                        else canPass = false;
                     }
-                }
+                    if(canPass) {
+                        answer = 1;
+                    }
 
-                if(canPass){
-                    answer = 1;
-                    System.out.println(answer);
-                    return;
+                    canPass = true;
+                    for(int l = 0 ; l < N ; l++){
+                        if(arr[l][0] == i || arr[l][0] == j || arr[l][1] == k){
+                            canPass = false;
+                            break;
+                        }
+                    }
+                    if(canPass) {
+                        answer = 1;
+                    }
+
+                    canPass = true;
+                    for(int l = 0 ; l < N ; l++){
+                        if(arr[l][0] == i || arr[l][1] == j || arr[l][1] == k){
+                            canPass = false;
+                            break;
+                        }
+                    }
+                    if(canPass) {
+                        answer = 1;
+                    }
+
+                    canPass = true;
+                    for(int l = 0 ; l < N ; l++){
+                        if(arr[l][1] == i || arr[l][1] == j || arr[l][1] == k){
+                            canPass = false;
+                            break;
+                        }
+                    }
+                    if(canPass) {
+                        answer = 1;
+                    }
                 }
             }
         }
