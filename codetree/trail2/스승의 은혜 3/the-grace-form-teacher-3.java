d012818097951912be65da5db42232b28931ba6c
Sehyun Jin <niki8533@naver.com>
2026-05-26 15:07:29 +0900

    [Passed] 스승의 은혜 3 (239ms, 14MB)

--- codetree/trail2/스승의 은혜 3/the-grace-form-teacher-3.java
+++ codetree/trail2/스승의 은혜 3/the-grace-form-teacher-3.java
@@ -3,41 +3,44 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException{
-        // Please write your code here.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int B = Integer.parseInt(st.nextToken());
-
-        int[][] arr = new int[N][2];
-        for(int i = 0 ; i < N ; i++){
+        int n = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[n][2];
+        
+        for(int i = 0; i < n; i++){
             st = new StringTokenizer(br.readLine());
             arr[i][0] = Integer.parseInt(st.nextToken());
             arr[i][1] = Integer.parseInt(st.nextToken());
         }
 
-        Arrays.sort(arr, (o1, o2) ->{
-            return (o1[0]+o1[1]) - (o2[0]+o2[1]);
+        // Please write your code here.
+        Arrays.sort(arr, (o1, o2) -> {
+            int first = o1[0] + o1[1];
+            int second = o2[0] + o2[1];
+
+            return first - second;
         });
 
         int max = 0;
-        for(int i = 0 ; i < N ; i++){
-            int sum = B - ((arr[i][0] / 2) + arr[i][1]);
+        for(int i = 0 ; i < n ; i++){
+            int sum = b - ((arr[i][0] / 2) + arr[i][1]);
             int count = 1;
-            for(int j = 0 ; j < N ; j++){
+            for(int j = 0 ; j < n ; j++){
                 if(i == j){
                     continue;
                 }
-                if(sum - (arr[j][0] + arr[j][1]) < 0){
+
+                sum -= (arr[j][0] + arr[j][1]);
+
+                if(sum < 0){
                     break;
                 }
-                else {
-                    sum -= (arr[j][0] + arr[j][1]);
-                    count++;
-                }
+
+                count++;
             }
 
-            //System.out.println(count);
             max = Math.max(max, count);
         }
 
