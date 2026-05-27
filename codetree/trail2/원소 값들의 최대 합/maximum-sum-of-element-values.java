addaeccbdaba74e32b9c7662130a6058d2f320c8
Sehyun Jin <niki8533@naver.com>
2026-05-27 19:17:41 +0900

    [Passed] 원소 값들의 최대 합 (89ms, 9MB)

---
+++ codetree/trail2/원소 값들의 최대 합/maximum-sum-of-element-values.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        // Please write your code here.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N + 1];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 1 ; i <= N ; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int max = 0;
+        for(int i = 1 ; i <= N ; i++){
+            int temp = arr[i];
+            int sum = 0;            
+            for(int j = 0 ; j < M ; j++){
+                temp = arr[temp];
+                sum += temp;
+            }
+            max = Math.max(max, sum);
+        }
+
+        System.out.println(max);
+    }
+}
