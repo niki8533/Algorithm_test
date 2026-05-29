755a1daf9000614923093548b1b2ba1a1e9617b5
Sehyun Jin <niki8533@naver.com>
2026-05-29 16:14:53 +0900

    [Wrong Answer] 수들의 최대 차 (97ms, 9MB)

---
+++ codetree/trail2/수들의 최대 차/maximum-difference-in-numbers.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        // Please write your code here.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+        for(int i = 0 ; i < N ; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        int max = 1;
+        for(int i = 1 ; i < N ; i++){
+            if(Math.abs(arr[0] - arr[i]) <= K){
+                max = Math.max(max, (i + 1));
+            }
+        }
+
+        System.out.println(max);
+    }
+}
