b2d5cf13931578cf206a5374d34bfaa6def13d35
Sehyun Jin <niki8533@naver.com>
2026-05-29 13:53:46 +0900

    [Wrong Answer] 가장 작은 x 찾기 (85ms, 9MB)

---
+++ codetree/trail2/가장 작은 x 찾기/find-smallest-x.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[][] arr = new int[N][2];
+        for(int i = 0 ; i < N ; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        int min = Integer.MAX_VALUE;
+        for(int i = 1 ; i <= 10 ; i++){
+            boolean isIn = true;
+            int temp = i;
+            for(int j = 0 ; j < N ; j++){
+                temp *= 2;
+                
+                if(arr[j][0] > temp || temp > arr[j][1]){
+                    isIn = false;
+                    break;
+                }
+            }
+
+            if(isIn){
+                min = Math.min(min, i);
+            }
+        }
+
+        System.out.println(min);
+    }
+}
