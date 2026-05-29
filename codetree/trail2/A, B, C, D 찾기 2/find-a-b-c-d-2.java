34b8df478b27f58bfaaa9baeb742d82ff3662cfa
Sehyun Jin <niki8533@naver.com>
2026-05-29 18:25:45 +0900

    [Wrong Answer] A, B, C, D 찾기 2 (111ms, 9MB)

---
+++ codetree/trail2/A, B, C, D 찾기 2/find-a-b-c-d-2.java
@@ -0,0 +1,58 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<Integer> list = new ArrayList<>();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0 ; i < 15 ; i++){
+            list.add(Integer.parseInt(st.nextToken()));
+        }
+
+        Collections.sort(list);
+
+        for(int a = 1 ; a <= 7 ; a++){
+            for(int b = a ; b <= 8 ; b++){
+                for(int c = b ; c <= 9 ; c++){
+                    for(int d = c ; d <= 10 ; d++){
+                        ArrayList<Integer> temp = new ArrayList<>();
+                        temp.add(a);
+                        temp.add(b);
+                        temp.add(c);
+                        temp.add(d);
+
+                        temp.add(a + b);
+                        temp.add(a + c);
+                        temp.add(a + d);
+                        temp.add(b + c);
+                        temp.add(b + d);
+                        temp.add(c + d);
+
+                        temp.add(a + b + c);
+                        temp.add(a + b + d);
+                        temp.add(a + c + d);
+                        temp.add(b + c + d);
+                        
+                        temp.add(a + b + c + d);
+
+                        Collections.sort(temp);
+
+                        boolean isTrue = true;
+                        for(int i = 0 ; i < 15; i++){
+                            if(list.get(i) != temp.get(i)){
+                                isTrue = false;
+                                break;
+                            }
+                            
+                        }
+                        if(isTrue){
+                            System.out.println(a + " " + b + " " + c + " " + d);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
