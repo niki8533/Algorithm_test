7d3c1d749d67d341d547072f5aeee318a494e6f3
Sehyun Jin <niki8533@naver.com>
2026-05-29 17:48:35 +0900

    [Passed] 훌륭한 점프 (94ms, 9MB)

--- codetree/trail2/훌륭한 점프/great-jump.java
+++ codetree/trail2/훌륭한 점프/great-jump.java
@@ -19,7 +19,7 @@ public class Main {
         }
 
         int minmax = Integer.MAX_VALUE;
-        for(int i = Math.max(arr[0], arr[N-1]) ; i <= max ; i++){
+        for(int i = Math.max(arr[0], arr[N-1]) ; i <= 100 ; i++){
             if(isPossible(i, arr)){
                 minmax = Math.min(minmax, i);
             }
@@ -37,7 +37,7 @@ public class Main {
                     return false;
                 }
 
-                lastIndex++;
+                lastIndex = i;
             }
         }
 
