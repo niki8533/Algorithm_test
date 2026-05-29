5553bda2ed603e8d7bce9fd962b7840f78473f71
Sehyun Jin <niki8533@naver.com>
2026-05-29 15:30:11 +0900

    [Passed] 가장 많이 나온 쌍 (103ms, 9MB)

--- codetree/trail2/가장 많이 나온 쌍/the-most-frequent-pair.java
+++ codetree/trail2/가장 많이 나온 쌍/the-most-frequent-pair.java
@@ -9,26 +9,23 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        int[][] arr = new int[M][2];
+        HashMap<Integer, Integer> map = new HashMap<>();
         for(int i = 0 ; i < M ; i++){
             st = new StringTokenizer(br.readLine());
-            arr[i][0] = Integer.parseInt(st.nextToken());
-            arr[i][1] = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            int temp = 0;
+            if(a > b){
+                temp = (b * 10) + a;
+            } else temp = (a * 10) + b;
+
+            map.put(temp, map.getOrDefault(temp, 0) + 1);
         }
 
-        Arrays.sort(arr, (o1, o2) -> {
-            if(o1[0] == o2[0]){
-                return o1[1] - o2[1];
-            } else return o1[0] - o2[0];
-        });
-
         int max = 0;
-        int count = 1;
-        for(int i = 0 ; i < M - 1 ; i++){
-            if(arr[i][0] != arr[i+1][0] || arr[i][1] != arr[i][1]){
-                max = Math.max(max, count);
-                count = 1;
-            } else count++;
+        for(int key : map.keySet()){
+            max = Math.max(max, map.get(key));
         }
 
         System.out.println(max);
