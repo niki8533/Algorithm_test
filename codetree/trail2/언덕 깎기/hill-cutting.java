b1d53f47e9e828afe7d041245c6083b11c64b30d
Sehyun Jin <niki8533@naver.com>
2026-06-01 17:47:49 +0900

    [Wrong Answer] 언덕 깎기 (94ms, 9MB)

---
+++ codetree/trail2/언덕 깎기/hill-cutting.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
+        for(int i = 0 ; i < N ; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        int min = Integer.MAX_VALUE;
+        for(int i = 0 ; i < N ; i++){
+            int low = arr[i];
+            int high = arr[i] + 17;
+            int cost = 0;
+            for(int j = 0 ; j < N ; j++){
+                if(arr[j] < low){
+                    cost += (low - arr[j]) * (low - arr[j]);
+                } else if(arr[j] > high){
+                    cost += (arr[j] - high) * (arr[j] - high);
+                }
+            }
+            min = Math.min(min, cost);
+        }
+        System.out.println(min);
+    }
+}
