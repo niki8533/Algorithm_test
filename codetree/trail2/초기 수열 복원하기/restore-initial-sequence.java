f4c329637868bed52a5426103372ac042aebdb5d
Sehyun Jin <niki8533@naver.com>
2026-06-01 15:43:16 +0900

    [Passed] 초기 수열 복원하기 (150ms, 10MB)

--- codetree/trail2/초기 수열 복원하기/restore-initial-sequence.java
+++ codetree/trail2/초기 수열 복원하기/restore-initial-sequence.java
@@ -19,14 +19,14 @@ public class Main {
             list.add(i);
             for(int j = 0 ; j < N - 1 ; j++){
                 int next = Math.abs(list.get(j) - plus[j]);
-                if(list.contains(next)){
+                if(list.contains(next) || next == 0){
                     break;
                 }
 
                 list.add(next);
             }
 
-            if(list.size() == 5){
+            if(list.size() == N){
                 StringBuilder sb = new StringBuilder();
                 for(int j = 0 ; j < N ; j++){
                     sb.append(list.get(j) + " ");
