cedf1cf75488cba72e010620b21cb7508df92269
Sehyun Jin <niki8533@naver.com>
2026-06-01 15:39:46 +0900

    [Runtime Error] 초기 수열 복원하기 (96ms, 9MB)

---
+++ codetree/trail2/초기 수열 복원하기/restore-initial-sequence.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] plus = new int[N - 1];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0 ; i < N - 1 ; i++){
+            plus[i] = Integer.parseInt(st.nextToken());
+        }
+
+        
+        for(int i = 1 ; i <= N ; i++){
+            List<Integer> list = new ArrayList<>();
+
+            list.add(i);
+            for(int j = 0 ; j < N - 1 ; j++){
+                int next = Math.abs(list.get(j) - plus[j]);
+                if(list.contains(next)){
+                    break;
+                }
+
+                list.add(next);
+            }
+
+            if(list.size() == 5){
+                StringBuilder sb = new StringBuilder();
+                for(int j = 0 ; j < N ; j++){
+                    sb.append(list.get(j) + " ");
+                }
+                System.out.println(sb);
+                return;
+            }
+        }
+    }
+}
