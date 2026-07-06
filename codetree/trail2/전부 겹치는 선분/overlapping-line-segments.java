81f2dc563d7df311e063cb052c6d1e55d84f6f42
Sehyun Jin <niki8533@naver.com>
2026-07-06 18:06:42 +0900

    [Passed] 전부 겹치는 선분 (96ms, 9MB)

---
+++ codetree/trail2/전부 겹치는 선분/overlapping-line-segments.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[101];
+
+        int min = Integer.MAX_VALUE;
+        int max = 0;
+        for(int i = 0 ; i < N ; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            for(int j = x1 ; j <= x2 ; j++){
+                arr[j]++;
+            }
+
+            min = Math.min(min, x1);
+            max = Math.max(max, x2);
+        }
+
+        boolean isOverlap = false;
+        for(int i = min ; i <= max ; i++){
+            if(arr[i] >= N){
+                isOverlap = true;
+                break;
+            }
+        }
+
+        if(isOverlap){
+            System.out.println("Yes");
+        } else System.out.println("No");
+    }
+}
