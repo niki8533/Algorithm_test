364cb49dd9fd703c48968aee1220ef21968eedfc
Sehyun Jin <niki8533@naver.com>
2026-07-07 17:00:20 +0900

    [Wrong Answer] 코딩톡 (135ms, 10MB)

---
+++ codetree/trail2/코딩톡/coding-talk.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int p = sc.nextInt();
+        char[] c = new char[M];
+        int[] u = new int[M];
+        for (int i = 0; i < M; i++) {
+            c[i] = sc.next().charAt(0);
+            u[i] = sc.nextInt();
+        }
+        // Please write your code here.
+        HashSet<Character> set = new HashSet<>();
+        for(int i = 0 ; i < N ; i++){
+            set.add((char)('A' + i));
+        }
+        
+        for(int i = p - 1 ; i < M ; i++){
+            if(set.contains(c[i])){
+                set.remove(c[i]);
+            }
+        }
+
+        for(char ch : set){
+            System.out.print(ch + " ");
+        }
+    }
+}
