8048ea97f3de597b86e4a96c34be174517c9f594
Sehyun Jin <niki8533@naver.com>
2026-07-08 14:17:34 +0900

    [Wrong Answer] 연속된 숫자 만들기 2 (124ms, 10MB)

---
+++ codetree/trail2/연속된 숫자 만들기 2/create-consecutive-numbers-2.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[3];
+        arr[0] = sc.nextInt();
+        arr[1] = sc.nextInt();
+        arr[2] = sc.nextInt();
+        // Please write your code here.
+
+        Arrays.sort(arr);
+
+        int count = 0;
+        if((arr[1] - arr[0]) > 2 && (arr[1]+1) == arr[2]){
+            count = 2;
+        }
+        else if((arr[2] - arr[1]) > 2 && (arr[0]+1) == arr[1]){
+            count = 2;
+        }
+        else if((arr[2] - arr[1]) > 2 && (arr[1] - arr[0]) > 2){
+            count = 2;
+        }
+        else if((arr[2] - arr[1]) > 2 || (arr[1] - arr[0]) > 2){
+            count = 2;
+        }
+        else if((arr[1] - arr[0]) == 2 || (arr[2] - arr[1])== 2){
+            count = 1;
+        }
+
+        System.out.println(count);
+    }
+}
