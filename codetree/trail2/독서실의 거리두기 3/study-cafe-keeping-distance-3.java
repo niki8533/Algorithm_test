fdf8d2e9bbfd3c2568eefa334a9a5261d4b6bb72
Sehyun Jin <niki8533@naver.com>
2026-07-14 16:14:33 +0900

    [Passed] 독서실의 거리두기 3 (131ms, 10MB)

--- codetree/trail2/독서실의 거리두기 3/study-cafe-keeping-distance-3.java
+++ codetree/trail2/독서실의 거리두기 3/study-cafe-keeping-distance-3.java
@@ -1,53 +1,52 @@
-import java.util.*;
-import java.io.*;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        String seats = sc.next();
         // Please write your code here.
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
         int[] arr = new int[N];
-        String str = br.readLine();
         for(int i = 0 ; i < N ; i++){
-            char c = str.charAt(i);
+            char c = seats.charAt(i);
+
             arr[i] = c - 48;
         }
 
-        int maxmin = Integer.MAX_VALUE;
-        int max = 0;
+        int maxDistance = 0;
+        int maxI = 0;
+        int maxJ = 0;
         for(int i = 0 ; i < N ; i++){
-            if(arr[i] == 1){
-                continue;
-            }
-
-            int[] temp = new int[N];
-            for(int j = 0 ; j < N ; j++){
-                if(i == j){
-                    temp[j] = 1;
-                } else temp[j] = arr[j];
-            }
-
-            max = Math.max(max, distance(temp));
-        }
-        
-
-        System.out.println(max);
-    }
-
-    public static int distance(int[] arr){
-        int min = Integer.MAX_VALUE;
-
-        for(int i = 0 ; i < arr.length ; i++){
             if(arr[i] == 0) continue;
 
-            for(int j = i + 1 ; j < arr.length ; j++){
+            for(int j = i + 1 ; j < N ; j++){
                 if(arr[j] == 1){
-                    min = Math.min(min, (j - i));
+                    if((j - i) > maxDistance){
+                        maxDistance = j - i;
+
+                        maxI = i;
+                        maxJ = j;
+                    }
+
                     break;
                 }
             }
         }
 
-        return min;
+        arr[(maxI + maxJ) / 2] = 1;
+
+        int answer = Integer.MAX_VALUE;
+        for(int i = 0 ; i < N ; i++){
+            if(arr[i] == 0) continue;
+
+            for(int j = i + 1 ; j < N ; j++){
+                if(arr[j] == 1){
+                    answer = Math.min(answer, (j - i));
+                    break;
+                }
+            }
+        }
+
+        System.out.println(answer);
     }
 }
