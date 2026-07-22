9bbde5610c816ca0e3416e66d8ddad4a4b278ec8
Sehyun Jin <niki8533@naver.com>
2026-07-22 16:32:33 +0900

    [Wrong Answer] 세 수의 최대 곱 (142ms, 10MB)

---
+++ codetree/trail2/세 수의 최대 곱/maximum-product-of-three-numbers.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        // Please write your code here.
+
+        Arrays.sort(arr);
+
+        if(arr[0] * arr[1] > arr[n - 3] * arr[n-2]){
+            System.out.println(arr[0]*arr[1]*arr[n-1]);
+        } else {
+            System.out.println(arr[n-3]*arr[n-2]*arr[n-1]);
+        }
+    }
+}
