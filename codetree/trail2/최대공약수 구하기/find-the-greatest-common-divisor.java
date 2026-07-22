ac80fa60886bb8f92295be9928c06d32cc46b05b
Sehyun Jin <niki8533@naver.com>
2026-07-22 18:11:15 +0900

    [Wrong Answer] 최대공약수 구하기 (128ms, 10MB)

---
+++ codetree/trail2/최대공약수 구하기/find-the-greatest-common-divisor.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        // Please write your code here.
+        System.out.println(max(n, m));
+    }
+
+    public static int max(int n, int m){
+        int divisor = 1;
+        int max = 0;
+        while(divisor < n || divisor < m){
+            if(n % divisor == 0 && m % divisor == 0){
+                max = Math.max(max, divisor);
+            }
+            divisor++;
+        }
+
+        return max;
+    }
+}
