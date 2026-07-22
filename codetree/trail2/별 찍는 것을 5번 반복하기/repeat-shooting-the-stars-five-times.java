4376364949c94ea7305b5176de8e3ed3d7bf9c06
Sehyun Jin <niki8533@naver.com>
2026-07-22 17:16:20 +0900

    [Passed] 별 찍는 것을 5번 반복하기 (92ms, 9MB)

---
+++ codetree/trail2/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+
+        for(int i = 0 ; i < 5 ; i++){
+            printStar();
+        }
+
+    }
+
+    public static void printStar(){
+        System.out.println("**********");
+    }
+}
