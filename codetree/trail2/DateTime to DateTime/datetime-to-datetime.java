57d2b967c9f176c8a54503fd82a4081986b49912
Sehyun Jin <niki8533@naver.com>
2026-08-03 15:59:32 +0900

    [Wrong Answer] DateTime to DateTime (96ms, 9MB)

--- codetree/trail2/DateTime to DateTime/datetime-to-datetime.java
+++ codetree/trail2/DateTime to DateTime/datetime-to-datetime.java
@@ -14,7 +14,7 @@ public class Main {
     }
 
     public static int dateTime(int a, int b, int c){
-        if(a > 11 || b > 11 || c > 11){
+        if(a >= 11 || b >= 11 || c >= 11){
             int time = 0;
             time += (a - 11) * 24 * 60;
             time += (b - 11) * 60;
