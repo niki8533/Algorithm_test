1891535e98b36c43b3b898535a313f9a5cdac309
Sehyun Jin <niki8533@naver.com>
2026-08-10 17:26:54 +0900

    [Passed] 요일 맞추기 (129ms, 10MB)

--- codetree/trail2/요일 맞추기/guess-day-of-week.java
+++ codetree/trail2/요일 맞추기/guess-day-of-week.java
@@ -1,37 +1,33 @@
-import java.util.*;
-import java.io.*;
-
+import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m1 = sc.nextInt();
+        int d1 = sc.nextInt();
+        int m2 = sc.nextInt();
+        int d2 = sc.nextInt();
         // Please write your code here.
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int m1 = Integer.parseInt(st.nextToken());
-        int d1 = Integer.parseInt(st.nextToken());
-        int m2 = Integer.parseInt(st.nextToken());
-        int d2 = Integer.parseInt(st.nextToken());
-
-        String[] week = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
         int[] dates = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        String[] week = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
 
-        int day1 = 0;
+        int day1 = d1;
+        int day2 = d2;
+
         for(int i = 0 ; i < m1 ; i++){
             day1 += dates[i];
         }
-        day1 += d1;
-        
-        int day2 = 0;
+
         for(int i = 0 ; i < m2 ; i++){
             day2 += dates[i];
         }
-        day2 += d2;
 
         int diff = (day2 - day1 + 1) % 7;
 
         if(diff < 0){
             diff += 7;
         }
+
         System.out.println(week[diff]);
     }
 }
