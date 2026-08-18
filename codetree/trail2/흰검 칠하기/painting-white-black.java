fc659bb1f0404c7dea171e2f8821e6ef2265ea7b
Sehyun Jin <niki8533@naver.com>
2026-08-18 18:20:47 +0900

    [Runtime Error] 흰검 칠하기 (136ms, 10MB)

---
+++ codetree/trail2/흰검 칠하기/painting-white-black.java
@@ -0,0 +1,52 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        // Please write your code here.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int offset = 1000;
+
+        int index = 0;
+        HashSet<Integer> set = new HashSet<>(); 
+        int[] wc = new int[100001];
+        int[] bc = new int[100001];
+        char[] color = new char[100001];
+        for(int i = 0 ; i < n ; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            char d = st.nextToken().charAt(0);
+
+            if(d == 'L'){
+                for(int j = (index - x) + 1 ; j <= index ; j++){
+                   wc[j + offset]++;
+                   color[j + offset] = 'W';
+                   set.add(j + offset);
+                }
+                index = (index - x) + 1;
+            } else {
+                for(int j = index ; j < (index + x) ; j++){
+                    bc[j + offset]++;
+                    color[j + offset] = 'B';
+                    set.add(j + offset);
+                }
+                index = (index + x) - 1;
+            }
+        }
+
+        int white = 0;
+        int black = 0;
+        int gray = 0;
+        for(int i : set){
+            if(wc[i] >= 2 && bc[i] >= 2){
+                gray++;
+            } else if(color[i] == 'W'){
+                white++;
+            } else if(color[i] == 'B'){
+                black++;
+            }
+        }
+        System.out.println(white + " " + black + " " + gray);
+    }
+}
