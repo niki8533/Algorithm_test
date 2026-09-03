c47e1faee7aabb1402085073dc9855a1ee2615ae
Sehyun Jin <niki8533@naver.com>
2026-09-03 16:43:59 +0900

    [Wrong Answer] 연속되는 수 3 (126ms, 10MB)

---
+++ codetree/trail2/연속되는 수 3/continuous-number3.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+        
+        // Please write your code here.
+        Stack<Integer> stack = new Stack<>();
+        int count = 1;
+        int max = 1;
+        boolean isNegative = false;
+        for(int i = 0 ; i < n ; i++){
+            boolean temp = false;
+
+            if(arr[i] < 0){
+                temp = true;
+            } else temp = false;
+
+            if(isNegative == temp){
+                count++;
+                stack.add(arr[i]);
+            } else {
+                isNegative = temp;
+                count = 1;
+            }
+            max = Math.max(max, count);
+        }
+
+        System.out.println(max);
+    }
+}
