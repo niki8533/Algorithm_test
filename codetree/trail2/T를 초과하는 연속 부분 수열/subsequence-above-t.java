e6647c60a1fb12edbb08c2a130add2119fb2b36a
Sehyun Jin <niki8533@naver.com>
2026-09-03 17:26:29 +0900

    [Wrong Answer] T를 초과하는 연속 부분 수열 (146ms, 10MB)

---
+++ codetree/trail2/T를 초과하는 연속 부분 수열/subsequence-above-t.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int t = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+        // Please write your code here.
+        int max = 0;
+        Stack<Integer> s = new Stack<>();
+        int index = 0;
+        for(int i = 0 ; i < n ; i++){
+            if(arr[i] >= t){
+                s.add(arr[i]);
+                index = i;
+                break;
+            }
+        }
+
+        if(s.isEmpty()){
+            System.out.println(0);
+            return;
+        }
+
+        for(int i = index ; i < n ; i++){
+            if(arr[i] < s.peek() && arr[i] >= t){
+                max = Math.max(max, s.size());
+                s = new Stack<>();
+                s.add(arr[i]);
+            } else if(arr[i] > s.peek()){
+                s.add(arr[i]);
+            } else continue;
+        }
+
+        max = Math.max(max, s.size());
+        System.out.println(max);
+    }
+}
