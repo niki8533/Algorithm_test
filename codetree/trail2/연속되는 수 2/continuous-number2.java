780b3563816baa0ad4a59379171eabcc3b2ab2bc
Sehyun Jin <niki8533@naver.com>
2026-09-03 16:12:06 +0900

    [Passed] 연속되는 수 2 (200ms, 13MB)

--- codetree/trail2/연속되는 수 2/continuous-number2.java
+++ codetree/trail2/연속되는 수 2/continuous-number2.java
@@ -11,9 +11,9 @@ public class Main {
         }
         // Please write your code here.
         int count = 1;
-        int max = 0;
-        for(int i = 1 ; i < n ; i++){
-            if(arr[i - 1] == arr[i]){
+        int max = 1;
+        for(int i = 0 ; i < n - 1 ; i++){
+            if(arr[i] == arr[i + 1]){
                 count++;
                 
             } else count = 1;
